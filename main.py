df293a44d2070486aa7802b324cb55b039e02ec4
superPilipala <576688920@qq.com>
2023-07-25 12:54:44 +0800

    添加Hex。csv的内容

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ if __name__ == '__main__':
     ic = IChing()
     div = ic.get_divinatory()
     # if not div:
-    print(div.model_dump())
-    print(div.get_content())
+    # print(div.model_dump())
+    # print(div.get_content())
+    print(div.name)
